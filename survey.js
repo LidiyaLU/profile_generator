a2b4da222a62602eec357438abd43a6b82e60e11
LidiyaLU <cheerrlidiya@yandex.ru>
2020-11-02 22:20:27 +0000

    add new stupid questions to my survey

--- survey.js
+++ survey.js
@@ -8,9 +8,13 @@ const rl = readline.createInterface({
 
 rl.question('What is your name? ', (nameX) => {
   rl.question('What is your superpower? ', (factX) => {
-      
-      console.log("My name is "+ nameX + " and I can " + factX);
+    rl.question('What is your hobby?', (hobby) => {
+      rl.question('If you had dinosaur how would you call it?', (dino) => {
+
+      console.log(`My name is ${nameX} and I can ${factX} I am fond of ${hobby}. I have a dino ${dino}, Sounds nice, eh?`);
      
       rl.close();
+      });
+    });
   });
 });
